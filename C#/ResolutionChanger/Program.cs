737dfcba16a2ba0e175e4516020f01e67d173751
Mariola Wójcik <mwojcik95@gmail.com>
2020-05-08 11:58:56 +0200

    Added program for changing resolution of a picture

---
+++ C#/ResolutionChanger/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Size resizedPicResolution = new Size();
+            string inputDirPath = Directory.GetCurrentDirectory();
+            string outputDirPath = Directory.GetCurrentDirectory() + "\\output";
+
+            if (args.Length >=1 && args.Length <=3)
+            {
+                foreach(var param in args)
+                {
+                    string parameter = param.Substring(0, param.LastIndexOf('=')); 
+                    if (parameter.Equals("-res"))
+                    {
+                        resizedPicResolution = GetResolution(param);
+                    }
+                    else if(parameter.Equals("-inputdir"))
+                    {
+                        inputDirPath = GetDirectory(param);
+                    }
+                    else if (parameter.Equals("-outputdir"))
+                    {
+                        outputDirPath = GetDirectory(param);
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("Enter parameters!\nRequired: -res\nOptional: -inputdir -outputdir");
+                Environment.Exit(0);
+            }
+
+            if (resizedPicResolution.IsEmpty)
+            {
+                Console.Write("-res parameter is required!");
+                Environment.Exit(0);
+            }
+
+            Resizer resizer = new Resizer(inputDirPath, outputDirPath, resizedPicResolution);
+            List<string> pictureFiles = resizer.SelectPictures();
+            resizer.ResizePicture(pictureFiles);
+        }
+
+
+        private static Size GetResolution(string param) 
+        {
+            string tempSize = param.Substring(param.LastIndexOf('=') + 1);
+            int width = Convert.ToInt32(tempSize.Substring(0, tempSize.LastIndexOf('x')));
+            int height = Convert.ToInt32(tempSize.Substring(tempSize.LastIndexOf('x') + 1));
+
+           
+            Size resizedPicResolution = new Size(width, height); 
+            return resizedPicResolution; 
+  
+        }
+
+        private static string GetDirectory(string param)
+        {
+            string dirPath = param.Substring(param.LastIndexOf('=') + 1);
+            if (!Directory.Exists(dirPath))
+            {
+                Console.Write("Directory does not exist!");
+                Environment.Exit(0);
+            }
+            return dirPath;
+        }
+    }
+}
---
+++ C#/ResolutionChanger/Resizer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionChanger
+{
+    class Resizer
+    {
+        private string inputDirPath;
+        private string outputDirPath;
+        private Size resizedPicResolution; 
+        
+        public Resizer(string input, string output, Size res)
+        {
+            this.inputDirPath = input;
+            this.outputDirPath = output;
+            this.resizedPicResolution = res;
+
+            if (outputDirPath.Equals(Directory.GetCurrentDirectory() + "\\output"))
+            {
+                Directory.CreateDirectory(outputDirPath);
+            }
+
+        }
+
+        public List<string> SelectPictures()
+        {
+            List<string> pictureFiles = Directory.GetFiles(inputDirPath).ToList();
+
+            int allFiles = pictureFiles.Count - 1;
+
+            while (allFiles > 0)
+            {
+                string fileExtention = Path.GetExtension(pictureFiles[allFiles]);
+
+                if (!(fileExtention.Equals(".jpg") || fileExtention.Equals(".bmp")))
+                {
+                    pictureFiles.RemoveAt(allFiles);
+                }
+
+                allFiles--;
+            }
+
+            return pictureFiles; 
+        }
+
+        public void ResizePicture(List<string> pictureFiles)
+        {
+            foreach (string picturePath in pictureFiles)
+            {
+                Bitmap originalPic = (Bitmap)Image.FromFile(picturePath);
+                Bitmap resized = new Bitmap(originalPic, resizedPicResolution);
+
+                string copyPicName = GeneratePicName(picturePath);
+
+                resized.Save(copyPicName);
+            }
+        }
+
+        private string GeneratePicName(string picturePath)
+        {
+            string copyPicName = outputDirPath + "\\" + Path.GetFileName(picturePath);
+
+            int number = 1;
+            while (File.Exists(copyPicName))
+            {
+                copyPicName = outputDirPath + "\\" +
+                    Path.GetFileNameWithoutExtension(picturePath) +
+                    "_" + number + Path.GetExtension(picturePath);
+                number++;
+            }
+
+            return copyPicName;
+        }
+
+    }
+}
