9d32ec9a14b06da2e58f21f589bc1083b00fb585
Mariola Wójcik <mwojcik95@gmail.com>
2020-05-21 21:51:10 +0200

    Added generating table rows in loop for all person elements

--- XML/XML_DOM_parser.js
+++ XML/XML_DOM_parser.js
@@ -1,20 +1,38 @@
 function getPersonList() {
-    // loading XML document in browser
-    var xhttpReq = new XMLHttpRequest();
-
-    xhttpReq.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            getPersonList();
-        }
-    };
-
-    //loading XML document from the file to the XML DOM object
-    xhttpReq.open("GET", "person_list.xml", true);
+    const xhttpReq = new XMLHttpRequest();
+    //synchronic loading XML document from the file to the XML DOM object
+    xhttpReq.open("GET", "person_list.xml", false);
     xhttpReq.send();
 
-    var xmlDoc = xhttpReq.responseXML;
-    //getting the HTML table by ID
-    document.getElementById("personList").innerHTML =
-        xmlDoc.getElementsByTagName("person")[0].childNodes[0].nodeValue;
+    const xmlDoc = xhttpReq.responseXML;
+    const htmlTable = document.getElementById("personlistTable");
+    const personNodes = xmlDoc.getElementsByTagName("person");
 
+    let newText;
+    let cellContent;
+    let oneCell;
+
+    //generating table rows in loop for all person elements
+    for (let newRow = 0; newRow < personNodes.length; newRow++) {
+        const oneRow = htmlTable.insertRow(-1);
+        const cells = personNodes[newRow].children;
+        for (let newCell = 0; newCell < cells.length; newCell++) {
+            const childrenCells = cells[newCell].children;
+            if(childrenCells.length > 0){ //Node has children (not a leaf)
+                for(let newChildrenCell = 0; newChildrenCell < childrenCells.length; newChildrenCell++){
+                    // Insert a cell in the row at the end of the table
+                    oneCell = oneRow.insertCell(-1);
+                    // Append a text node to the cell
+                    cellContent = childrenCells[newChildrenCell].innerHTML;
+                    newText = document.createTextNode(cellContent);
+                    oneCell.appendChild(newText);
+                }
+            }else{ //Node is a leaf
+                oneCell = oneRow.insertCell(-1);
+                cellContent = cells[newCell].innerHTML;
+                newText = document.createTextNode(cellContent);
+                oneCell.appendChild(newText);
+            }
+        }
+    }
 }
