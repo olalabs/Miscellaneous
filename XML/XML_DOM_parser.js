b2974e7afd10fa737bcf2f0eff9a27f1fd6e645a
Mariola Wójcik <mwojcik95@gmail.com>
2020-05-21 23:31:07 +0200

    Split the code into functions

--- XML/XML_DOM_parser.js
+++ XML/XML_DOM_parser.js
@@ -8,31 +8,32 @@ function getPersonList() {
     const htmlTable = document.getElementById("personlistTable");
     const personNodes = xmlDoc.getElementsByTagName("person");
 
-    let newText;
-    let cellContent;
-    let oneCell;
-
     //generating table rows in loop for all person elements
     for (let newRow = 0; newRow < personNodes.length; newRow++) {
+        // Insert a row at the end of the table
         const oneRow = htmlTable.insertRow(-1);
-        const cells = personNodes[newRow].children;
-        for (let newCell = 0; newCell < cells.length; newCell++) {
-            const childrenCells = cells[newCell].children;
-            if(childrenCells.length > 0){ //Node has children (not a leaf)
-                for(let newChildrenCell = 0; newChildrenCell < childrenCells.length; newChildrenCell++){
-                    // Insert a cell in the row at the end of the table
-                    oneCell = oneRow.insertCell(-1);
-                    // Append a text node to the cell
-                    cellContent = childrenCells[newChildrenCell].innerHTML;
-                    newText = document.createTextNode(cellContent);
-                    oneCell.appendChild(newText);
-                }
-            }else{ //Node is a leaf
-                oneCell = oneRow.insertCell(-1);
-                cellContent = cells[newCell].innerHTML;
-                newText = document.createTextNode(cellContent);
-                oneCell.appendChild(newText);
-            }
+        var cells = personNodes[newRow].children;
+        for (var newCell = 0; newCell < cells.length; newCell++) {
+            checkDescendants(cells[newCell].children, oneRow)
         }
+    }
+
+    function checkDescendants(childrenCells, oneRow){
+        if(childrenCells.length > 0){ //Node has children (not a leaf)
+            for(let newChildrenCell = 0; newChildrenCell < childrenCells.length; newChildrenCell++){
+                generateCell(childrenCells[newChildrenCell], oneRow)
+            }
+        }else{ //Node is a leaf
+            generateCell(cells[newCell], oneRow)
+        }
+    }
+
+    function generateCell(indexCell, oneRow){
+        // Insert a cell in the row at the end of the table
+        let oneCell = oneRow.insertCell(-1);
+        // Append a text node to the cell
+        let cellContent = indexCell.innerHTML;
+        let newText = document.createTextNode(cellContent);
+        oneCell.appendChild(newText);
     }
 }
