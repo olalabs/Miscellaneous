e089d70353588af303d808d33729b7c55938f692
Mariola Wójcik <mwojcik95@gmail.com>
2020-06-22 16:50:19 +0200

    Initial project

---
+++ Spring/planetarySystem/src/main/java/com/example/planetarySystem/PlanetarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.planetarySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetarySystemApplication.class, args);
+	}
+
+}
---
+++ Spring/planetarySystem/src/test/java/com/example/planetarySystem/PlanetarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planetarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
