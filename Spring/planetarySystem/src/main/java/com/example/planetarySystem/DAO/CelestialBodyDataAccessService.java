8c12b95755433bf7e567a1565808c52693fb0123
Mariola Wójcik <mwojcik95@gmail.com>
2020-06-27 18:40:08 +0200

    Code to retrieve data from database and transform it to java object

--- Spring/planetarySystem/src/main/java/com/example/planetarySystem/DAO/CelestialBodyDataAccessService.java
+++ Spring/planetarySystem/src/main/java/com/example/planetarySystem/DAO/CelestialBodyDataAccessService.java
@@ -1,15 +1,24 @@
 package com.example.planetarySystem.DAO;
 
 import com.example.planetarySystem.model.CelestialBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres implementation")
 public class CelestialBodyDataAccessService implements CelestialBodyDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CelestialBodyDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertCelestialBody(UUID id, CelestialBody celestialBody) {
         return 0;
@@ -17,14 +26,27 @@ public class CelestialBodyDataAccessService implements CelestialBodyDao {
 
     @Override
     public List<CelestialBody> selectAllCelestialBody() {
-        List<CelestialBody> cb = new ArrayList<>();
-        cb.add(new CelestialBody(UUID.randomUUID(), "FROM POSTGRES DB"));
-        return cb;
+        final String sql = "SELECT id, name FROM celestialbody";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new CelestialBody(id, name);
+        });
     }
 
     @Override
     public Optional<CelestialBody> selectCelestialBodyById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM celestialbody WHERE id = ?";
+
+        CelestialBody celestialBody = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (resultSet, i) -> {
+            UUID celestialBodyId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new CelestialBody(celestialBodyId, name);
+        });
+        return Optional.ofNullable(celestialBody);
     }
 
     @Override
